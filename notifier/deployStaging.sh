6cc06c30b2f244290ea3d43e052b74d1908cce24
Ty J Everett <ty@tyweb.us>
2025-03-11 16:09:01 -0700

    Deploy

--- notifier/deployStaging.sh
+++ notifier/deployStaging.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud functions deploy stagingNotifier --runtime=nodejs14 --env-vars-file=staging.functions.env.yaml --entry-point=notifier --timeout=540 --trigger-event=google.storage.object.finalize --trigger-resource=staging-hashbrown.babbage.systems --memory=4096 --source .
+gcloud functions deploy stagingNotifier --runtime=nodejs20 --env-vars-file=staging.functions.env.yaml --entry-point=notifier --timeout=540 --trigger-event=google.storage.object.finalize --trigger-resource=staging-hashbrown.babbage.systems --memory=4096 --source .
