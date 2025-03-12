ce351e0fe7bcc9501562fe7e767117ae2ef1ff26
Ty J Everett <ty@tyweb.us>
2025-03-11 17:00:23 -0700

    Node header limit

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ if [ $NODE_ENV = 'production' ] || [ $NODE_ENV = 'staging' ]
 then
   npm run build
   echo "$GCP_STORAGE_CREDS" > /app/out/storage-creds.json
-  node ./out/src/index.js
+  node --max-http-header-size=512000 ./out/src/index.js
   exit
 fi
 
