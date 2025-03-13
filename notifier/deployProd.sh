d99fdb68fc50908199739062149166f872cdbf63
Ty J Everett <ty@tyweb.us>
2025-03-13 09:17:02 -0700

    Update prod

--- notifier/deployProd.sh
+++ notifier/deployProd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud functions deploy prodNotifier --gen2 --runtime=nodejs22 --env-vars-file=prod.functions.env.yaml --entry-point=notifier --timeout=540 --trigger-event=google.storage.object.finalize --trigger-resource=hashbrown.babbage.systems --memory=4096 --source .
+gcloud functions deploy prodNotifier --gen2 --runtime=nodejs22 --env-vars-file=prod.functions.env.yaml --entry-point=notifier --timeout=540 --trigger-event=google.storage.object.finalize --trigger-location=us --trigger-resource=hashbrown.babbage.systems --memory=4096 --source .
