d658aeab205f387fb90a27dcf056577dc29f222a
Jackie Lu <jackielu3@gmail.com>
2025-04-10 19:01:57 -0700

    us

--- notifier/deployProd.sh
+++ notifier/deployProd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcloud functions deploy prodNotifier --gen2 --runtime=nodejs22 --env-vars-file=prod.functions.env.yaml --entry-point=notifier --timeout=540 --region=us-central1 --trigger-event=google.storage.object.finalize --trigger-location=us --trigger-resource=staging-uhrp
+gcloud functions deploy prodNotifier --gen2 --runtime=nodejs22 --env-vars-file=prod.functions.env.yaml --entry-point=notifier --timeout=540 --region=us --trigger-event=google.storage.object.finalize --trigger-location=us --trigger-resource=staging-uhrp
  --memory=4096 --source .
