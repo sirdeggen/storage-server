b32f4992a2dd9cd8516d36b80ca8199229c0b247
Jackie Lu <jackielu3@gmail.com>
2025-04-15 09:10:15 -0700

    deploy

--- notifier/deployStaging.sh
+++ notifier/deployStaging.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo ${secrets.GCP_BUCKET_NAME}
-
-gcloud functions deploy stagingNotifier --gen2 --runtime=nodejs22 --env-vars-file=staging.functions.env.yaml --entry-point=notifier --timeout=540 --region=us-central1 --trigger-event=google.storage.object.finalize --trigger-resource=secrets.GCP_BUCKET_NAME --memory=4096 --source .
+gcloud functions deploy stagingNotifier \
+    --gen2 \
+    --runtime=nodejs22 \
+    --env-vars-file=staging.functions.env.yaml \
+    --entry-point=notifier \
+    --timeout=540 \
+    --region=us-central1 \
+    --trigger-event=google.storage.object.finalize \
+    --trigger-resource=$GCP_BUCKET_NAME \
+    --memory=4096 \
+    --source .
