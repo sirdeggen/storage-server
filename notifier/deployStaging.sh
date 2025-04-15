ed432857c59981727c7d95e0a3e71c22160b5a42
Jackie Lu <jackielu3@gmail.com>
2025-04-15 09:01:56 -0700

    testicle

--- notifier/deployStaging.sh
+++ notifier/deployStaging.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcloud functions deploy stagingNotifier --gen2 --runtime=nodejs22 --env-vars-file=staging.functions.env.yaml --entry-point=notifier --timeout=540 --region=us-central1 --trigger-event=google.storage.object.finalize --trigger-resource=${ secrets.GCP_BUCKET_NAME } --memory=4096 --source .
+echo ${secrets.GCP_BUCKET_NAME}
+
+gcloud functions deploy stagingNotifier --gen2 --runtime=nodejs22 --env-vars-file=staging.functions.env.yaml --entry-point=notifier --timeout=540 --region=us-central1 --trigger-event=google.storage.object.finalize --trigger-resource=secrets.GCP_BUCKET_NAME --memory=4096 --source .
